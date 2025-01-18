04d3a35e1248a0086e0ffbae9d35bf7357b1d779
Rodrigo Vittori <vittorirodrigo@gmail.com>
2025-01-18 14:36:14 -0300

    [M4.L1] Carrera de tortugas - Actividad # 6 "Moviendo a las dos"
    Agregamos variables de seguimiento, pedimos predicción del resultado al usuario e implementamos lógica de movimiento y fin de carrera

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 """
-# [M4.L1] Carrera de tortugas - Actividad # 5 "Tercer Jugador"
+# [M4.L1] Carrera de tortugas - Actividad # 6 "Moviendo a las dos"
 
-Objetivo: Agregar nuestra tercer participante
+Objetivo: Configurar el avance de nuestras tortugas
+NOTA: Vamos a implementar el movimiento para las TRES (resolvemos el próximo ejercicio tmb)
+
+Paso Nº 1: Crear variable que contenga la distancia que debe recorrer una tortuga para ganar
+Paso Nº 2: Agregar bucle donde pidamos predicción del resultado al jugador
+Paso Nº 3: Agregamos variables que registren el avance de cada tortuga
+Paso Nº 4: Agregamos el bucle de la carrera: while(not tenemos_ganadora)
+-> incluír random
+Paso Nº 5: Agregamos una condición para mostrar el resultado de la carrera
 
 """
 
 import turtle
+import random
 
 # VARIABLES GLOBALES
 velocidad_corredores = 5 # Controla la velocidad de la animación (no a la que "corren", sino a la que las dibujamos)
@@ -39,12 +48,14 @@ for pista in range(1, (1 + cant_secciones_pista)):
     t.seth(0)                   # Acomoda la tortuga para que quede mirando a la dcha
     t.fd(ancho_secciones_pista) # Avanza el ancho hasta la próxima sección
 
+t.hideturtle()
 ###################################
 
 # CREAR CORREDORES:
 
 # PRIMERA
 cant_tortugas += 1
+distancia_recorrida_tortuga_1 = 0
 primera = turtle.Turtle() # ROJA
 primera.color("crimson")
 primera.shape("turtle")
@@ -56,6 +67,7 @@ primera.speed(velocidad_corredores)
 
 # SEGUNDA
 cant_tortugas += 1
+distancia_recorrida_tortuga_2 = 0
 segunda = turtle.Turtle() # AZUL
 segunda.color("navy")
 segunda.shape("turtle")
@@ -67,6 +79,7 @@ segunda.speed(velocidad_corredores)
 
 # TERCERA
 cant_tortugas += 1
+distancia_recorrida_tortuga_3 = 0
 tercera = turtle.Turtle() # AMARILLA
 tercera.color("gold")
 tercera.shape("turtle")
@@ -75,5 +88,68 @@ tercera.penup()
 #tercera.goto(-230, 0)
 tercera.goto(x_inicial_tortugas, (y_inicial_tortugas - (distancia_entre_tortugas * (cant_tortugas - 1))))
 tercera.speed(velocidad_corredores)
+
+###################################
+# PEDIR PREDICCION USUARIO
+
+opcion_jugador = 0 # Vble global pero la declaramos acá por legibilidad del código
+
+while (opcion_jugador <= 0 or opcion_jugador > cant_tortugas):
+    opcion_jugador = int(input("Cual tortuga cree que ganara? (1/2/3): "))
+
+#opcion_jugador = input("Cual tortuga cree que ganara?: ")
+t.penup()
+t.goto(x_inicial_tortugas + 100, 120)
+t.write("Apostaste por la tortuga #" + str(opcion_jugador))
+#t.write("Apostaste por la tortuga " + opcion_jugador)
+
+tenemos_ganadora = False # Vble de control que detendrá el bucle de la carrera cuando haya una ganadora
+tortuga_ganadora = 0     # Vable que contiene la id de nuestra ganadora
+primera.pendown()
+segunda.pendown()
+tercera.pendown()
+
+while (not tenemos_ganadora):
+    
+    if (distancia_recorrida_tortuga_1 > ((cant_secciones_pista + 1) * ancho_secciones_pista)):
+        # Si la primera tortuga pasó la línea de meta:
+        tortuga_ganadora = 1
+        tenemos_ganadora = True
+        
+    elif (distancia_recorrida_tortuga_2 > ((cant_secciones_pista + 1) * ancho_secciones_pista)):
+        # Si la segunda tortuga pasó la línea de meta:
+        tortuga_ganadora = 2
+        tenemos_ganadora = True
+    
+    elif (distancia_recorrida_tortuga_3 > ((cant_secciones_pista + 1) * ancho_secciones_pista)):
+        # Si la segunda tortuga pasó la línea de meta:
+        tortuga_ganadora = 3
+        tenemos_ganadora = True
+    
+    else:
+        
+        # MOVIMIENTO/AVANCE TORTUGAS
+        
+        paso_tortuga = random.randint(1, velocidad_corredores) # VELOCIDAD DE AVANCE DE LA TORTUGA
+        primera.fd(paso_tortuga)
+        distancia_recorrida_tortuga_1 += paso_tortuga
+        
+        paso_tortuga = random.randint(1, velocidad_corredores) # VELOCIDAD DE AVANCE DE LA TORTUGA
+        segunda.fd(paso_tortuga)
+        distancia_recorrida_tortuga_2 += paso_tortuga
+        
+        paso_tortuga = random.randint(1, velocidad_corredores) # VELOCIDAD DE AVANCE DE LA TORTUGA
+        tercera.fd(paso_tortuga)
+        distancia_recorrida_tortuga_3 += paso_tortuga
+
+### FIN DE LA CARRERA ###
+
+# MOSTRAR TORTUGA GANADORA:
+t.penup()
+t.goto(x_inicial_tortugas + 100, -80)
+win_text = "¡La tortuga #" + str(tortuga_ganadora) + " ha ganado!"
+t.write(win_text)
+
+# To-do: agregar condición que compare nuestra predicción con el resultado
 
 # Colorín colorado el código ha terminado :D
